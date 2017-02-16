a85b3384a6fe16690c626a84b22835dbaab291d1
wanghaiboisgood <821871522@qq.com>
2017-02-17 00:02:04 +0800

    数据库表格调用

--- del.php
+++ del.php
@@ -1,11 +1,13 @@
 <?php
- include "db.php";
- $id=$_GET["id"];
- $db->query("delete from stu where id=".$id);
- if($db->affected_rows>0){
-     echo "<script>alert('删除成功');location.href='1.php'</script>";
- }
-
-
-
-?>
+	include "db.php";
+	$id=$_GET["id"];
+	$db->query("delete from students where id=".$id);
+	if($db->affected_rows>0){
+		?>
+		<script>
+		alert("删除成功");
+		location.assign("ajaxtable.php"); 
+		</script>			
+	<?php	
+	}
+	?>	
