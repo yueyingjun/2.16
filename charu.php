400e4682f8e339c6176f358c5de678d6e6529584
wangzechang2017 <www992710157@163.com>
2017-02-16 23:41:07 +0800

    王泽昌
    https://github.com/wangzechang2017/2.16

---
+++ charu.php
@@ -0,0 +1,9 @@
+<?php
+	header("Content-Type:text/html;charset=utf-8");
+	$mysql=new mysqli("localhost","root","","class");
+	$mysql->query("set names utf8");
+	$result=$mysql->query("insert into stu (name,age,sex) value ('','','')");
+	
+?><script type="text/javascript">
+	location.href="table.php";
+</script>
