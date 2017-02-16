4f2e9b3ffe226d95bf30f27237c7977e8c73ef22
sulin <email>
2017-02-16 23:30:19 +0800

    mysql

--- mysql.php
+++ mysql.php
@@ -1,59 +1,65 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <style>
-    table{
-      width:600px;margin:0 auto;
-      border:1px solid #000;
-      border-collapse: collapse;
-    }
-    td,th{
-      border:1px solid #000;
-    }
-    .add{
-      display: block;
-      width:598px;border:1px solid #000;
-      height:30px;
-      border-top:none;text-align: center;
-      line-height:30px ;margin:0 auto;
-      text-decoration: none;
-    }
-  </style>
-</head>
-<body>
-  <table>
-    <tr>
-      <th>名字</th>
-      <th>年龄</th>
-      <th>性别</th>
-      <th>操作</th>
-    </tr>
-    <?php
-      include "public.php";
-      $sql="select * from student";
-      $result=$aa->query($sql);
-      while ($row=$result->fetch_assoc()){
-    ?>
 
-          <tr>
-              <td><?php echo $row["name"]?></td>
-              <td><?php echo $row["age"]?></td>
-              <td><?php echo $row["sex"]?></td>
-              <td>
-                <a href="del.php?id=<?php echo $row['id']?>">删除</a> <a href="edit.php?id=<?php echo $row['id']?>">编辑</a></td>
-          </tr>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title></title>
+	</head>
+<style>
+	table{
+		width:500px;
+		border:1px solid #000;
+		margin:0 auto;
+		border-collapse: collapse;   /*合并为一个单一的边框*/
+	} 
+	td,th{
+		width:25%;
+		border:1px solid #000;
+	}
+	.add{
+		display: block;
+		width:498px;
+		height:20px;
+		border:1px solid #000;
+		border-top:0px solid #000;
+		text-decoration: none;
+		text-align: center;
+		line-height: 20px;
+		margin:0 auto;
+	}
+	td a{
+		line-height:20px;
+		text-align:center;
+	}
+</style>
+<body>
+	<table>
+		<tr>
+			<th>姓名</th>
+			<th>性别</th>
+			<th>年龄</th>
+			<th>操作</th>
+		</tr>
+		<?php
+          include "public.php";
+          $table="select * from student";
+          $result=$aa->query($table);
+          $row=$result->fetch_assoc();
+          while ($row){
+         ?>
+
+        <tr>
+          <td><?php echo $row["name"]?></td>
+          <td><?php echo $row["age"]?></td>
+          <td><?php echo $row["sex"]?></td>
+          <td>
+            <a href="del.php?id=<?php echo $row['id']?>">删除</a> </td>
+        </tr>
 
     <?php
       }
       ?>
-
-
-  </table>
-  <a href="" class="add">+</a>
+	</table>
+	<a href="" class="add">+</a>
 </body>
 </html>
