20632b4f59edfaf7efa57ebdec3b7abb3710e9d7
wuyawei <2499115366@qq.com>
2017-02-17 08:27:31 +0800

    吴亚卫
    https://github.com/wuyawei/2.16

--- del.php
+++ del.php
@@ -1,11 +1,9 @@
 <?php
- include "db.php";
- $id=$_GET["id"];
- $db->query("delete from stu where id=".$id);
- if($db->affected_rows>0){
-     echo "<script>alert('删除成功');location.href='1.php'</script>";
- }
-
-
-
+	include "db.php";
+	$id=$_GET["id"];
+	$sql="delete from stu where id=".$id;
+	$db->query($sql);
+	if($db->affected_rows>0){
+		echo "<script>alert('删除成功');location.href='table.php'</script>";
+	}
 ?>
