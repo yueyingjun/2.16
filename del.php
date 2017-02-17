8c3aa03ba83db4299039128ba8751409d27a46be
limbobark <limbobark@gmail.com>
2017-02-17 11:39:23 +0800

    update

--- del.php
+++ del.php
@@ -2,7 +2,7 @@
     include "contact.php";
     $id=$_GET["id"];
     $db->query("delete from person where id=$id");
-    while($db->affected_rows){
+    if($db->affected_rows){
         echo "<script>location.href='table.php'</script>";
     }
 ?>
