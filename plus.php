b4f8b1d90ea2afd2c9a0797d04216a09efad8310
limbobark <limbobark@gmail.com>
2017-02-17 10:24:29 +0800

    add travis ci

---
+++ plus.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+	<title>date plus</title>
+</head>
+<style>
+.box{
+
+}
+.btn{
+
+}
+</style>
+<body>
+	<form action="">
+	<input type="姓名">
+	<input type="年龄">
+	<input type="性别">
+	<input type="操作">
+	</form>
+	<button value="">确认添加</button>
+</body>
+</html>
